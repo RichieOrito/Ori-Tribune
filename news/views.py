11f4453d71ecfbdb74b667092c5ca56c5180501f
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-02 00:43:53 +0300

    This commit updates the new article

--- news/views.py
+++ news/views.py
@@ -94,8 +94,7 @@ def new_article(request):
             article = form.save(commit=False)
             article.editor = current_user
             article.save()
-        return redirect('NewsToday')
-
+        return redirect('newsToday')
     else:
         form = NewArticleForm()
     return render(request, 'new_article.html', {"form": form})
