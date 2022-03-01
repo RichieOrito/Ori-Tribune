6017652be676712a6251b46c189b4ed7537c1c81
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-02 00:43:06 +0300

    This commit removes the editor

--- news/admin.py
+++ news/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Editor,Article,tags, NewsLetterRecipients
+from .models import Article,tags, NewsLetterRecipients
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
 
-admin.site.register(Editor)
+# admin.site.register(Editor)
 admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
 admin.site.register(NewsLetterRecipients)
